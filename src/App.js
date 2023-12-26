ac322f0a39f2d8b232ef3462a3db59d3e28cb4a6
yiolon <hanhx777@gmail.com>
2023-12-26 22:48:15 +1300

    slide bar and input change count and reset

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
+    </div>
+  );
+}
+
+function Counter(props) {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const handleMinusClick = () => {
+    setStep(step - 1);
+  };
+  const handleAddClick = () => {
+    setStep(step + 1);
+  };
+
+  const handleMinusCountBtn = () => {
+    setCount(count - step);
+  };
+  const handleAddCountBtn = () => {
+    setCount(count + step);
+  };
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  const handleResetBtn = () => {
+    setCount(0);
+    setStep(1);
+  };
+
+  const formatDate = date.toLocaleString();
+  return (
+    <div>
+      <div>
+        <button onClick={handleMinusClick}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={handleAddClick}>+</button>
+      </div>
+      <div>
+        <button onClick={handleMinusCountBtn}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={handleAddCountBtn}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span> {formatDate}</span>
+      </p>
+      <button onClick={handleResetBtn}>reset</button>
     </div>
   );
 }
